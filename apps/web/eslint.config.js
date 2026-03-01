30a3a5a59386b2b6c409b035154b4fccd285fe24
Nikos Dritsakos <121593919+nikos118@users.noreply.github.com>
2026-02-28 23:49:34 -0800

    adwdwadawdwadwa

--- apps/web/eslint.config.js
+++
@@ -1,7 +0,0 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
-
-/** @type {import("eslint").Linter.Config[]} */
-export default [
-  { ignores: ["convex/_generated/**"] },
-  ...nextJsConfig,
-];
